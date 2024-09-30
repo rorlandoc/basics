9ab5415bb0003eb55b8c1f8b4a844d18e8122eb8
Rui Costa <rorlandoc@gmail.com>
2024-09-30 18:52:28 +0100

    change format settings

--- include/rc/array.hpp
+++ include/rc/array.hpp
@@ -4,36 +4,42 @@
 #include <cstddef>
 #include <stdexcept>
 
-namespace rc {
+namespace rc
+{
 
 template <typename Data, size_t Size>
-class array {
-   public:
-    array(Data &&init_value) {
-        for (size_t i = 0; i < Size; i++) {
+class array
+{
+  public:
+    array(Data &&init_value)
+    {
+        for (size_t i = 0; i < Size; i++)
+        {
             m_data[i] = init_value;
         }
     }
 
     constexpr size_t size() { return Size; }
 
-    Data &operator[](size_t index) {
+    Data &operator[](size_t index)
+    {
         if (is_index_valid(index)) return m_data[index];
         throw std::runtime_error{"[rc::array] Index out of range."};
     }
 
-    const Data &operator[](size_t index) const {
+    const Data &operator[](size_t index) const
+    {
         if (is_index_valid(index)) return m_data[index];
         throw std::runtime_error{"[rc::array] Index out of range"};
     }
 
-   protected:
+  protected:
     bool is_index_valid(size_t index) { return (index >= 0 && index < Size); }
 
-   private:
+  private:
     Data m_data[Size];
 };
 
-}  // namespace rc
+} // namespace rc
 
-#endif  // !RC_ARRAY_HPP
+#endif // !RC_ARRAY_HPP
